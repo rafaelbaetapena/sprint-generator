733492a48e97971c2295ce4bf67f01f0135b0ec5
Rafael Baeta Pena <rafaelrbncp@gmail.com>
2020-10-26 14:49:24 -0300

    commit inicial do projeto

---
+++ src/main/kotlin/com/rafaelbaetapena/sprintgenerator/SprintGeneratorApplication.kt
@@ -0,0 +1,11 @@
+package com.rafaelbaetapena.sprintgenerator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SprintGeneratorApplication
+
+fun main(args: Array<String>) {
+	runApplication<SprintGeneratorApplication>(*args)
+}
---
+++ src/test/kotlin/com/rafaelbaetapena/sprintgenerator/SprintGeneratorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rafaelbaetapena.sprintgenerator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SprintGeneratorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
